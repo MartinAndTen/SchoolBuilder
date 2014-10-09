6c09189404c3b96489e3a58ef053b30d5143a22c
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-09 13:56:57 +0200

    glömde includa

---
+++ SchoolLib/Education.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLib
+{
+    public class Education : IEducation
+    {
+        public string Teacher
+        {
+            get
+            {
+                return Teacher;
+            }
+            set
+            {
+                Teacher = value;
+            }
+        }
+
+        public string Students
+        {
+            get
+            {
+                return Students;
+            }
+            set
+            {
+                Teacher = value;
+            }
+        }
+
+        public string Feedback
+        {
+            get
+            {
+                return Feedback;
+            }
+            set
+            {
+                Feedback = value;
+            }
+        }
+    }
+}
