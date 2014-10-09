a7764daf79c0cb9a708fcde013a71d72130bd5ff
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-10-09 10:34:28 +0200

    Initial commit

---
+++ SchoolTest/TestEducationBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SchoolTest
+{
+    [TestClass]
+    public class TestEducationBuilder
+    {
+        [TestMethod]
+        public void JensenBuilderTest()
+        {
+            //Arrange
+
+            //Actual
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void NackaBuilderTest()
+        {
+            //Arrange
+
+            //Actual
+
+            //Assert
+        }
+    }
+}
